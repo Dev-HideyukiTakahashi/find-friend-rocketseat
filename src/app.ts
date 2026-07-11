e160b59ab5d3fee1255204bff691ce97e95159f0
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2026-07-11 08:18:27 -0300

    chore: config swagger and swagger ui

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,33 @@
-import { fastify } from 'fastify';
+import fastify from 'fastify';
+import { registerSwagger } from './config/swagger.js';
+import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod';
+import { z } from 'zod';
+import type { ZodTypeProvider } from 'fastify-type-provider-zod';
 
 export const app = fastify();
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+// registros
+await registerSwagger(app);
+
+app.withTypeProvider<ZodTypeProvider>().get(
+  '/foo',
+  {
+    schema: {
+      tags: ['test'],
+      summary: 'Rota de teste',
+      response: {
+        200: z.object({
+          message: z.string(),
+        }),
+      },
+    },
+  },
+  async () => {
+    return {
+      message: 'foo funcionando',
+    };
+  },
+);
---
+++ src/config/swagger.ts
@@ -0,0 +1,25 @@
+import swaggerUi from '@fastify/swagger-ui';
+import type { FastifyInstance } from 'fastify';
+import swagger from '@fastify/swagger';
+
+export async function registerSwagger(app: FastifyInstance) {
+  await app.register(swagger, {
+    openapi: {
+      openapi: '3.0.0',
+      info: {
+        title: 'FindAFriend API',
+        description: 'Documentação da API do projeto FindAFriend',
+        version: '1.0.0',
+      },
+      servers: [{ url: 'http://localhost:3000', description: 'Local' }],
+      tags: [
+        { name: 'orgs', description: 'Operações de organizações' },
+        { name: 'pets', description: 'Operações de pets' },
+      ],
+    },
+  });
+
+  await app.register(swaggerUi, {
+    routePrefix: '/docs',
+  });
+}
