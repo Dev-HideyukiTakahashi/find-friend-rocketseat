8796a4110b0cf9bdc16fbb4e887729d50221159e
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2026-07-12 00:20:49 -0300

    chore: config prisma and migration with tables

---
+++ prisma/migrations/20260712030415_create_table_pet_and_org/migration.sql
@@ -0,0 +1,53 @@
+-- CreateEnum
+CREATE TYPE "Age" AS ENUM ('PUPPY', 'YOUNG', 'ADULT', 'SENIOR');
+
+-- CreateEnum
+CREATE TYPE "Size" AS ENUM ('SMALL', 'MEDIUM', 'LARGE');
+
+-- CreateEnum
+CREATE TYPE "Independence" AS ENUM ('LOW', 'MEDIUM', 'HIGH');
+
+-- CreateEnum
+CREATE TYPE "Environment" AS ENUM ('SMALL', 'LARGE');
+
+-- CreateTable
+CREATE TABLE "orgs" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password_hash" TEXT NOT NULL,
+    "whatsapp" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "latitude" DECIMAL(10,8) NOT NULL,
+    "longitude" DECIMAL(11,8) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "orgs_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "pets" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "about" TEXT,
+    "age" "Age" NOT NULL,
+    "size" "Size" NOT NULL,
+    "energy_level" INTEGER NOT NULL,
+    "independence" "Independence" NOT NULL,
+    "environment" "Environment" NOT NULL,
+    "requirements" TEXT[],
+    "photos" TEXT[],
+    "org_id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "pets_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "orgs_email_key" ON "orgs"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "orgs_whatsapp_key" ON "orgs"("whatsapp");
+
+-- AddForeignKey
+ALTER TABLE "pets" ADD CONSTRAINT "pets_org_id_fkey" FOREIGN KEY ("org_id") REFERENCES "orgs"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import { registerSwagger } from './config/swagger.js';
 import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod';
 import { z } from 'zod';
 import type { ZodTypeProvider } from 'fastify-type-provider-zod';
+import { prisma } from './lib/prisma.js';
 
 export const app = fastify();
 
@@ -26,6 +27,20 @@ app.withTypeProvider<ZodTypeProvider>().get(
     },
   },
   async () => {
+    await prisma.pet.create({
+      data: {
+        name: 'Rex',
+        about: 'Um cachorro muito amigável.',
+        age: 'ADULT',
+        size: 'MEDIUM',
+        energyLevel: 3,
+        independence: 'MEDIUM',
+        environment: 'LARGE',
+        requirements: ['Precisa de quintal', 'Gosta de crianças'],
+        photos: ['url-da-foto-1.jpg'],
+        orgId: 'ID-DA-ONG-CADASTRADA-NO-BANCO',
+      },
+    });
     return {
       message: 'foo funcionando',
     };
